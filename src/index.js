98f1084c4ead5d86e1216d3151dec2f62d90ea2a
RUnexpert <aldali911@gmail.com>
2021-08-20 13:27:36 +0300

    feat: temporary solution

--- src/index.js
+++ src/index.js
@@ -4,7 +4,21 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    let brackets = 0;
+    for (let i = 0; i < expr.length; i++) {
+        if (expr[i] == '(') brackets++;
+        if (expr[i] == ')') brackets--;
+    };
+
+    if (brackets !== 0) {
+        throw new Error ('ExpressionError: Brackets must be paired');
+    };
+
+    if (expr.includes('/ 0')) {
+        throw new Error ('TypeError: Division by zero.');
+    };
+
+    return new Function(`return ${expr}`)();
 }
 
 module.exports = {
